84159047b5b3c97ca01826d5fab91b3b821fd07e
Amit Yadav <amit931552@gmail.com>
2025-06-07 01:28:19 +0530

    fsdklnlkjdf

---
+++ src/String/AddStrings.java
@@ -0,0 +1,15 @@
+package String;
+
+import java.util.Scanner;
+
+public class AddStrings {
+    static  void addString(){
+
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+    }
+}
