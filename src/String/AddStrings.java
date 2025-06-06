6b1124d981f09aa7b29a1acfbcea4d88c18d9e05
Amit Yadav <amit931552@gmail.com>
2025-06-07 01:32:47 +0530

    gjksd

--- src/String/AddStrings.java
+++ src/String/AddStrings.java
@@ -3,13 +3,23 @@ package String;
 import java.util.Scanner;
 
 public class AddStrings {
-    static  void addString(){
+    static  void addString(String a,int n){
+        String a=sc.nextInt();
+
+        for(int i=0; i<n; i++){
+
+        }
+
 
 
     }
 
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
+        String str=sc.next();
+        String str1=sc.next();
+
+
 
     }
 }
