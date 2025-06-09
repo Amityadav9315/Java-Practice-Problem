1b715fa05b0d73eaab3d1a093aac4f84f8d4f0d9
Amit Yadav <amit931552@gmail.com>
2025-06-10 01:47:44 +0530

    sdflhkklgsh

--- src/Array/RotateArray.java
+++ src/Array/RotateArray.java
@@ -8,14 +8,13 @@ public class RotateArray {
 
     static void rotateArray(int[] arr,int n,int k){
         int[] arr1=new int[n];  int index=0;
-         for(int i=k;i<n;i++){
+         for(int i=k; i<n;i++){
 
            arr1[index++]=arr[i];
            }
-        for(int i=0;i<n; i++){
-             arr1[index++]=arr[i];
-         }
-         for(int x: arr1){
+         for(int i=0; i<k;i++){
+          arr1[index++]=arr[i]; }
+        for(int x: arr1){
             System.out.println(x);
 }
 }   public static void main(String[] args) {
