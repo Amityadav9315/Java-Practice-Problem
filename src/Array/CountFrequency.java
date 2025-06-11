2f7f2ab9a8011204a7ad41ed8028472c203af631
Amit Yadav <amit931552@gmail.com>
2025-06-11 16:17:07 +0530

    vdskblvsd

--- src/Array/CountFrequency.java
+++ src/Array/CountFrequency.java
@@ -4,22 +4,45 @@ import java.util.Scanner;
 
 public class CountFrequency {
 
-    static  void countFrequency(int arr,int n){
-        int[] arr1=new int[n];
+    static void countFrequency(int[] arr, int n) {
+        int[] arr1 = new int[n];
 
-        int frequency=-1;
+        int visited = -1;
+        for (int i = 0; i < n; i++) {
 
+            int count = 1;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] == arr[j]) {
+                    count++;
+                    arr1[j] = visited;
 
+                }
+            }
+            if (arr1[i] != visited) {
+                arr1[i] = count;
 
+            }
 
     }
-
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        int[] arr=new int[n];
-        for(int i=0; i<n; i++){
-            arr[i]=sc.nextInt();
+            for (int i = 0; i < n; i++) {
+                if (arr1[i] != visited) {
+                    System.out.println(arr[i] +" "+ arr1[i]);
+                }
+                System.out.println();
+            }
         }
-    }
-}
+
+
+
+            public static void main (String[] args){
+                Scanner sc = new Scanner(System.in);
+                int n = sc.nextInt();
+                int[] arr = new int[n];
+                for (int i = 0; i < n; i++) {
+                    arr[i] = sc.nextInt();
+                }
+                countFrequency(arr,n);
+            }
+        }
+
+
