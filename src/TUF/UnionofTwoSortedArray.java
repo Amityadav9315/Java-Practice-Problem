6a42c3f35b5532e1bb0913c91adb5c84b2fb13e3
Amit Yadav <amit931552@gmail.com>
2025-06-21 17:52:53 +0530

    svfbm,vfs

--- src/TUF/UnionofTwoSortedArray.java
+++ src/TUF/UnionofTwoSortedArray.java
@@ -1,14 +1,28 @@
 package TUF;
 
+import java.util.Scanner;
+
 public class UnionofTwoSortedArray {
 
-    static void unionogSortedArray(){
+    static void unionogSortedArray(int[] arr1,int[] arr2,int n,int m){
 
 
 
     }
 
     public static void main(String[] args) {
-        Scanner
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int m=sc.nextInt();
+        int[] arr1=new int[n];
+        for(int i=0; i<n; i++){
+            arr1[i]=sc.nextInt();
+        }
+
+        int[] arr2=new int[m];
+        for(int i=0; i<n; i++){
+            arr2[i]=sc.nextInt();
+        }
+
     }
 }
