a6c386e0fcb8bc344e9ec4f36269866079f65951
Amit Yadav <amit931552@gmail.com>
2025-06-22 16:43:26 +0530

    fsnn.sf

--- src/TUF/UnionofTwoSortedArray.java
+++ src/TUF/UnionofTwoSortedArray.java
@@ -8,14 +8,12 @@ public class UnionofTwoSortedArray {
         // Maximum possible size = n + m
         int[] result = new int[n + m];
         int index = 0;
-
         // Add elements from arr1
         for (int i = 0; i < n; i++) {
             if (index == 0 || result[index - 1] != arr1[i]) {
                 result[index++] = arr1[i];
             }
         }
-
         for (int i = 0; i < m; i++) {
             boolean isPresent = false;
             for (int j = 0; j < index; j++) {
@@ -37,20 +35,16 @@ public class UnionofTwoSortedArray {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         int n = sc.nextInt();
         int m = sc.nextInt();
-
         int[] arr1 = new int[n];
         for (int i = 0; i < n; i++) {
             arr1[i] = sc.nextInt();
         }
-
         int[] arr2 = new int[m];
         for (int i = 0; i < m; i++) {
             arr2[i] = sc.nextInt();
         }
-
         unionSortedArray(arr1, arr2, n, m);
     }
 }
