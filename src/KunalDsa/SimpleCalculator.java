84112fa748a03d7248ffaeb1f23765b2cb3fad60
Amit Yadav <amit931552@gmail.com>
2025-06-23 14:31:29 +0530

    bdgkndfk

---
+++ src/KunalDsa/SimpleCalculator.java
@@ -0,0 +1,22 @@
+package KunalDsa;
+
+import java.util.Scanner;
+
+public class SimpleCalculator {
+
+    static  void simpleCalculator(int ch){
+
+
+
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the operator");
+        char ch=sc.nextLine().charAt(0);
+        int n1=sc.nextInt();
+        int n2=sc.nextInt();
+
+    }
+}
