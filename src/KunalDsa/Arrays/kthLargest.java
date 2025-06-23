86941d1b11e630e07cb08571f56bfb389bfe871a
Amit Yadav <amit931552@gmail.com>
2025-06-23 21:18:36 +0530

    bdgkndfk

---
+++ src/KunalDsa/Arrays/kthLargest.java
@@ -0,0 +1,30 @@
+package KunalDsa.Arrays;
+
+import java.util.Scanner;
+
+public class kthLargest {
+
+    static  void kthlargest(int[] arr,int k,int n){
+
+
+
+
+
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        int k=sc.nextInt();
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0; i<n; i++){
+            arr[i]=sc.nextInt();
+
+
+        }
+    }
+
+
+}
