287e97bcc80d988a2730951ced66bcad590c3096
Amit Yadav <amit931552@gmail.com>
2025-06-23 14:23:12 +0530

    bdgkndfk

--- src/KunalDsa/countNoofDigitOccurance.java
+++ src/KunalDsa/countNoofDigitOccurance.java
@@ -5,7 +5,23 @@ import java.util.Scanner;
 public class countNoofDigitOccurance {
 
 
-    static  void countNoofDigitOccurance(int n){
+    static  void countNoofDigitOccurance(int countno,int n){
+
+        int rem=0;
+        int count=0;
+        while(n>0){
+            rem=n%10;
+            n=n/10;
+            if(rem==countno){
+                count++;
+            }
+        }
+        if(count==0){
+            System.out.println("Not found");
+        }
+        else{
+            System.out.println(count);
+        }
 
 
 
@@ -16,6 +32,8 @@ public class countNoofDigitOccurance {
 
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
+        int countno=sc.nextInt();
         int n=sc.nextInt();
+        countNoofDigitOccurance(countno,n);
     }
 }
