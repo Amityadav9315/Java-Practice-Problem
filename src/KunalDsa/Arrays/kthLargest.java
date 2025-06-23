96d3580b8488ac093b838dfd11fb7afba521ea78
Amit Yadav <amit931552@gmail.com>
2025-06-23 21:30:31 +0530

    bdgkndfk

--- src/KunalDsa/Arrays/kthLargest.java
+++ src/KunalDsa/Arrays/kthLargest.java
@@ -7,19 +7,22 @@ public class kthLargest {
     static  void kthlargest(int[] arr,int k,int n) {
 
         int max = 0;
-        int maxIndex = 0;
+
 
 
         for (int j = 1; j < k; j++) {
 
             max = 0;
+            int maxIndex = 0;
             for (int i = 0; i < n; i++) {
 
                 if (arr[i] > max) {
                     max = arr[i];
+                    maxIndex=i;
                 }
 
             }
+             arr[maxIndex]=0;
 
 
         }
@@ -43,6 +46,8 @@ public class kthLargest {
 
 
         }
+
+        kthlargest(arr,k,n);
     }
 
 
