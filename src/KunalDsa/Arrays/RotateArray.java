b9cdb70aaa9389542ac657cb7a0d5867c524b39d
Amit Yadav <amit931552@gmail.com>
2025-06-23 22:48:02 +0530

    bdgkndfk

--- src/KunalDsa/Arrays/RotateArray.java
+++ src/KunalDsa/Arrays/RotateArray.java
@@ -4,14 +4,31 @@ import java.util.Scanner;
 
 public class RotateArray {
 
-    static  void rotateArray(){
+    static  void rotateArray(int[] arr,int n,int k){
+
+        int[] arr1=new int[k];
+        int[] result=new int[n];
+        int index=0;
+        for(int i=0; i<k; i++){
+            arr1[i]=arr[i];
+        }
+        for(int i=0; i<n; i++)
+
+
+
+
 
 
     }
 
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
+        int k=sc.nextInt();
         int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0; i<n; i++){
+            arr[i]=sc.nextInt();
+        }
 
     }
 }
