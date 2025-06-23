3e6de74b0483ba5a997ed2d39aee18a713c89cca
Amit Yadav <amit931552@gmail.com>
2025-06-23 14:09:56 +0530

    bdgkndfk

--- src/KunalDsa/FibonacciSeries.java
+++ src/KunalDsa/FibonacciSeries.java
@@ -6,8 +6,16 @@ public class FibonacciSeries {
 
 
     static   void fibonacciSeries(int n){
+        int a=0;
+        int b=1;
 
-        for(int i=0; i<n; i++){
+
+        for(int i=0; i<2; i++){
+             int c=a+b;
+            System.out.println(c+" ");
+
+
+
 
         }
 
