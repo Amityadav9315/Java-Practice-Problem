7c1a1e16f40ef17a18fbf6a442122209235eef6b
Amit Yadav <amit931552@gmail.com>
2025-06-24 10:32:36 +0530

    fvbkvf

--- src/KunalDsa/Arrays/RotateArray.java
+++ src/KunalDsa/Arrays/RotateArray.java
@@ -4,28 +4,25 @@ import java.util.Scanner;
 
 public class RotateArray {
 
-    static  void rotateArray(int[] arr,int n,int k){
+    static  void rotateArray(int[] arr,int n,int k) {
 
-        int[] arr1=new int[k];
-        int[] result=new int[n];
-        int index=0;
-        for(int i=0; i<k; i++){
-            arr1[i]=arr[i];
+        int[] arr1 = new int[k];
+        int[] result = new int[n];
+        int index = 0;
+        for (int i = 0; i < k; i++) {
+            arr1[i] = arr[i];
         }
-        for(int i=k+1; i<n; i++){
-            result[index++]=arr[i];
+        for (int i = k + 1; i < n; i++) {
+            result[index++] = arr[i];
         }
-        for(int i=0; i<k; i++){
-            result[k-i]=arr[i];
+        for (int i = 0; i < k; i++) {
+            result[k - i] = arr[i];
 
         }
 
-        for(int x:result){
+        for (int x : result) {
             System.out.println(result);
         }
-        if(fruits.equals("mango")){
-
-
     }
 
     public static void main(String[] args) {
---
+++ src/KunalDsa/Switchstatement.java
@@ -0,0 +1,36 @@
+package KunalDsa;
+
+import java.util.Scanner;
+
+public class Switchstatement {
+
+
+    static void switchStatement(String fruit){
+        switch (fruit){
+            case "Mango":
+                System.out.println("king");
+                break;
+            case "Banana":
+                System.out.println("Queen");
+                break;
+            case "Papaya":
+                System.out.println("Legend");
+                break;
+            case "Orange":
+                System.out.println("Nagpur");
+                break;
+            default:
+                System.out.println("Invalid");
+        }
+
+
+
+
+    }
+
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        String  fruit=in.next();
+        switchStatement(fruit);
+    }
+}
