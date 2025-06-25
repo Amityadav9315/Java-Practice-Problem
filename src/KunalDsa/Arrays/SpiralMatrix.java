2fdf0a8130170ae9d19c44f832e697872dfa09c9
Amit Yadav <amit931552@gmail.com>
2025-06-25 15:37:51 +0530

    fnkhggf

--- src/KunalDsa/Arrays/SpiralMatrix.java
+++ src/KunalDsa/Arrays/SpiralMatrix.java
@@ -6,8 +6,14 @@ public class SpiralMatrix {
 
     static  void spiralMatrix(int[][] arr,int n,int m){
 
+        int c=m+n;
+        int[] result=new int[c];
+
         for(int i=0; i<n; i++){
             for(int i=0; i<m; i++){
+
+
+                arr[]
 
 
             }
