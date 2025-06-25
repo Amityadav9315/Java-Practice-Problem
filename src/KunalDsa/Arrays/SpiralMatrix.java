d530689805cad04fb72003820275ddd03c03b3d9
Amit Yadav <amit931552@gmail.com>
2025-06-25 15:49:20 +0530

    fnkhggf

--- src/KunalDsa/Arrays/SpiralMatrix.java
+++ src/KunalDsa/Arrays/SpiralMatrix.java
@@ -4,40 +4,49 @@ import java.util.Scanner;
 
 public class SpiralMatrix {
 
-    static  void spiralMatrix(int[][] arr,int n,int m){
+    static void spiralMatrix(int[][] arr, int n, int m) {
+        int top = 0, bottom = n - 1, left = 0, right = m - 1;
 
-        int c=m+n;
-        int[] result=new int[c];
+        while (top <= bottom && left <= right) {
+            for (int i = left; i <= right; i++) {
+                System.out.print(arr[top][i] + " ");
+            }
+            top++;
 
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
+            for (int i = top; i <= bottom; i++) {
+                System.out.print(arr[i][right] + " ");
+            }
+            right--;
 
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) {
+                    System.out.print(arr[bottom][i] + " ");
+                }
+                bottom--;
+            }
 
-                result[i]=arr[i][j];
-
-
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    System.out.print(arr[i][left] + " ");
+                }
+                left++;
             }
         }
-        for(int x:result){
-            System.out.print(x+" ");
-        }
-
-
     }
 
     public static void main(String[] args) {
-        Scanner  sc=new Scanner( System.in);
+        Scanner sc = new Scanner(System.in);
 
-        int n=sc.nextInt();
-        int m=sc.nextInt();
-        int[][]  arr=new int[n][m];
-        for(int i=0; i<n; i++){
-            for(int i=0; i<m; i++){
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[][] arr = new int[n][m];
 
-                arr[n][m]=sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = sc.nextInt();
             }
         }
 
-        spiralMatrix(arr,n,m);
+        spiralMatrix(arr, n, m);
     }
 }
