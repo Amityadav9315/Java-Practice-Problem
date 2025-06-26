c3b21660935212d85958fbd275bdf15bbfe7e6e0
Amit Yadav <amit931552@gmail.com>
2025-06-26 14:08:40 +0530

    gsrvnk

--- src/KunalDsa/NumberWithEvenNumberofDigit.java
+++ src/KunalDsa/NumberWithEvenNumberofDigit.java
@@ -7,6 +7,31 @@ public class NumberWithEvenNumberofDigit {
     static  void  evenNoofDigit(int[]  arr,int n){
 
         int count=0;
+        int count1=0;
+        int rem=0;
+        int c=0;
+
+        for(int i=0; i<n; i++){
+            c=arr[i];
+
+
+            while(c>0) {
+
+                rem = c % 10;
+                c=c/10;
+                count++;
+
+
+            }
+            if(count%2==0){
+                count1++;
+            }
+            c=0;
+            count=0;
+
+
+
+        }
 
 
 
