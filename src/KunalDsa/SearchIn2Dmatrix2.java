e0e90bc26b5149a24c41fcfdf7723763a36d41ee
Amit Yadav <amit931552@gmail.com>
2025-06-26 13:46:30 +0530

    bfkdfd

--- src/KunalDsa/SearchIn2Dmatrix2.java
+++ src/KunalDsa/SearchIn2Dmatrix2.java
@@ -5,22 +5,38 @@ import java.util.Scanner;
 public class SearchIn2Dmatrix2 {
 
 
-    static  void searchin2DMatrix(){
+    static void searchin2DMatrix(int[][] arr,int n,int m) {
+        int count=0;
+        int target=50;
 
+        for(int i=0; i<n; i++){
+
+            if(arr[n][m]==target){
+                count++;
+            }
+        }
+        if(count>=1){
+            System.out.println("Found");
+        }
+        else{
+            System.out.println("Not found");
+        }
 
 
     }
 
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        int m=sc.nextInt();
-        int[] [] arr=new int[n][m];
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[][] arr = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
 
-            arr[n][m]=sc.nextInt();
+                arr[n][m] = sc.nextInt();
+            }
+
         }
-
+        searchin2DMatrix(arr,n,m);
     }
 }
