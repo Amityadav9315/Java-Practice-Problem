42a9a60c0fac22713fbf4e2101723050c1dd708f
Amit Yadav <amit931552@gmail.com>
2025-06-26 14:30:21 +0530

    gsrvnk

--- src/KunalDsa/RichestCustomerWealth.java
+++ src/KunalDsa/RichestCustomerWealth.java
@@ -5,7 +5,16 @@ import java.util.Scanner;
 public class RichestCustomerWealth {
 
 
-    static  void richestCustomerWealth(){
+    static  void richestCustomerWealth(int[] [] arr,int n,int m){
+        int sum=0;
+        int max=0;
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+
+                sum=sum+arr[i][j];
+            }
+        }
 
 
     }
@@ -16,7 +25,13 @@ public class RichestCustomerWealth {
         int m=sc.nextInt();
 
         int[][]  arr=new int[n][m];
-        for(int i=0; i<n)
+        for(int i=0; i<n; i++){
+
+            for(int j=0; j<m; j++){
+
+                arr[i][j]=sc.nextInt();
+            }
+        }
 
     }
 }
