e38f6949eb286225400b6e0aeb2831d948dab31b
Amit Yadav <amit931552@gmail.com>
2025-06-26 13:42:39 +0530

    bfkdfd

--- src/KunalDsa/SearchIn2Dmatrix2.java
+++ src/KunalDsa/SearchIn2Dmatrix2.java
@@ -13,6 +13,14 @@ public class SearchIn2Dmatrix2 {
 
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int m=sc.nextInt();
+        int[] [] arr=new int[n][m];
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+
+            arr[n][m]=sc.nextInt();
+        }
 
     }
 }
