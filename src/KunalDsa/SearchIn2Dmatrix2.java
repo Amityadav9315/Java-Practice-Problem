88e1b25b340bbe1e17da5595f42f008fcb364ac2
Amit Yadav <amit931552@gmail.com>
2025-06-26 13:40:04 +0530

    bfkdfd

---
+++ src/KunalDsa/SearchIn2Dmatrix2.java
@@ -0,0 +1,18 @@
+package KunalDsa;
+
+import java.util.Scanner;
+
+public class SearchIn2Dmatrix2 {
+
+
+    static  void searchin2DMatrix(){
+
+
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+    }
+}
