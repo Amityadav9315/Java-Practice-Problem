31d38db86d765e8d9fc8e3b0f256a360cdb47382
Amit Yadav <amit931552@gmail.com>
2025-06-26 14:02:05 +0530

    gsrvnk

---
+++ src/KunalDsa/NumberWithEvenNumberofDigit.java
@@ -0,0 +1,25 @@
+package KunalDsa;
+
+import java.util.Scanner;
+
+public class NumberWithEvenNumberofDigit {
+
+    static  void  evenNoofDigit(int[]  arr,int n){
+
+        int count=0;
+
+
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+
+        int[]  arr=new int[n];
+        for(int i=0; i<n; i++){
+            arr[i]=sc.nextInt();
+
+        }
+    }
+}
