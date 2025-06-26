7afdbc793ee0ae5e6eee3a8ae57eb608b06c2b8b
Amit Yadav <amit931552@gmail.com>
2025-06-26 10:18:08 +0530

    vdsn.ksd

---
+++ src/KunalDsa/LinearSearchString.java
@@ -0,0 +1,30 @@
+package KunalDsa;
+
+import java.util.Scanner;
+
+public class LinearSearchString {
+
+    static  void stringSearch(String str){
+        int n=str.length();
+        String target="a";
+        int count=0;
+
+        for(int i=0; i<n; i++){
+            if(str.charAt(i)=="a"){
+
+            }
+
+
+
+        }
+
+
+
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String str=sc.next();
+    }
+}
