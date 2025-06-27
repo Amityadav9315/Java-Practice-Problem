21288cb4bca47d99adc3fa6b3e986d6176cc7e47
Amit Yadav <amit931552@gmail.com>
2025-06-27 10:24:30 +0530

    fevn;lebt

--- src/KunalDsa/BinarySearch/binarysearch.java
+++ src/KunalDsa/BinarySearch/binarysearch.java
@@ -1,14 +1,26 @@
 package KunalDsa.BinarySearch;
 
+import java.util.Scanner;
+
 public class binarysearch {
 
 
-    static  void binarySearch(){
+    static  void binarySearch(int[] arr ,int n){
+
+        int start=0;
+        int end= arr.length-1;
+
 
 
     }
 
     public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int [] arr=new int[n];
+        for(int i=0; i<n; i++){
 
+            arr[i]=sc.nextInt();
+        }
     }
 }
