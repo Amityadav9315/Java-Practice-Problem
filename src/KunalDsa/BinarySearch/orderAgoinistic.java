efa86f1d6035840adc55da2fd813ec41ac32cffc
Amit Yadav <amit931552@gmail.com>
2025-06-27 14:52:33 +0530

    sfkhgfgffd

--- src/KunalDsa/BinarySearch/orderAgoinistic.java
+++ src/KunalDsa/BinarySearch/orderAgoinistic.java
@@ -8,37 +8,36 @@ public class orderAgoinistic {
     static  void orderAgoinsitic(int[] arr,int n){
 
         int start=0;
-        int end=arr.length;
+        int end=arr.length-1;
         int  target=100;
 
-        for(int i=0; i<n; i++){
-            if(arr[i]<=arr[i+1]){
+            if(arr[0]<=arr[1]){
                 System.out.println("Ascending Order");
             }
             else{
                 System.out.println("Descending Order");
             }
-        }
+
 
         while(start<=end){
 
             int mid=start+(end-start)/2;
-            for(int i=0; i<n; i++){
 
-                if(target==mid){
+
+                if(target==arr[mid]){
                     System.out.println("Element found at index :"+mid);
                     break;
                 }
 
-               else  if(mid>target){
+               else  if(arr[mid]>target){
                     end=mid-1;
                 }
-                else if(mid<target){
+                else if(arr[mid]<target){
                     start=mid+1;
                 }
 
 
-            }
+
         }
 
 
@@ -51,5 +50,8 @@ public class orderAgoinistic {
         for(int i=0; i<n; i++){
             arr[i]=sc.nextInt();
         }
-    }
+
+        orderAgoinsitic(arr,n);
+
+        }
 }
