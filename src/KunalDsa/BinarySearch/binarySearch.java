bee9543f60cead91d4bc4b42504deef27bd8b534
Amit Yadav <amit931552@gmail.com>
2025-06-27 10:32:46 +0530

    sfkhgfgffd

--- src/KunalDsa/BinarySearch/binarySearch.java
+++ src/KunalDsa/BinarySearch/binarySearch.java
@@ -2,13 +2,30 @@ package KunalDsa.BinarySearch;
 
 import java.util.Scanner;
 
-public class binarysearch {
+public class binarySearch {
 
 
     static  void binarySearch(int[] arr ,int n){
 
         int start=0;
+        int target=50;
         int end= arr.length-1;
+        int mid=start+(end-start)/2;
+
+        while(start<=end){
+            if(target==arr[mid]){
+                System.out.println(mid);
+
+                }
+            if(target>arr[mid]){
+                start=mid+1;
+            }
+            if(target<arr[mid]){
+                end=mid-1;
+            }
+
+
+            }
 
 
 
