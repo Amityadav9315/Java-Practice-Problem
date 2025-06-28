2a50f27dfb268660aea1f07b1e72f332e922678a
Amit Yadav <amit931552@gmail.com>
2025-06-28 21:29:43 +0530

    sfkhgfgffd

---
+++ src/KunalDsa/BinarySearch/CelingOfaNumber.java
@@ -0,0 +1,35 @@
+package KunalDsa.BinarySearch;
+
+import java.util.Scanner;
+
+public class CelingOfaNumber {
+
+    static  void   cealingofaNumber(int[] arr,int n){
+
+        int min=0;
+        int start=0;
+        int end=arr.length-1;
+
+        for(int i=0; i<n; i++){
+
+            int mid=start+(end-start)/2;
+
+            if()
+        }
+
+
+
+
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int []  arr=new int[n];
+        for(int i=0; i<n; i++) {
+
+            arr[i]=sc.nextInt();
+        }
+    }
+}
