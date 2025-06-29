99ce6e3ae141a39b471ab05449ed02ef2fea81c0
Amit Yadav <amit931552@gmail.com>
2025-06-29 11:17:53 +0530

    fsblks

---
+++ src/KunalDsa/BinarySearch/InfiniteArray.java
@@ -0,0 +1,17 @@
+package KunalDsa.BinarySearch;
+
+import java.util.Scanner;
+
+public class InfiniteArray {
+
+    static  void infiniteArray(int[] arr,int target,int start,int end){
+
+
+
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
