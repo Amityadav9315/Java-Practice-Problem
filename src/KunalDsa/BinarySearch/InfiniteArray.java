995222c738a43d106233036ebbed8282aea95786
Amit Yadav <amit931552@gmail.com>
2025-06-29 15:17:51 +0530

    fn;lkg

--- src/KunalDsa/BinarySearch/InfiniteArray.java
+++ src/KunalDsa/BinarySearch/InfiniteArray.java
@@ -3,16 +3,12 @@ package KunalDsa.BinarySearch;
 import java.util.Scanner;
 
 public class InfiniteArray {
-
-
     public static void main(String[] args) {
 
         int[] arr={3,5,7,9,10,90,100,100,130,140,160,170};
         int target=10;
         System.out.println(ans(arr,target));
-
     }
-
     static int ans(int[] arr, int target) {
         int start = 0;
         int end = 1;
@@ -23,21 +19,14 @@ public class InfiniteArray {
         }
         return binarySearch(arr, target, start, end);
     }
-
-
     static int binarySearch(int[] arr, int target, int start, int end) {
-
         while (start <= end) {
 
             int mid = start + (end - start) / 2;
-
-
-
               if (target > arr[mid]) {
                 start = mid + 1;
             } else if (target < arr[mid]) {
                 end = mid - 1;
-
             }
             else  {
                 return mid;
