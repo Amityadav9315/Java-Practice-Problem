92fbeee19df1bdcfcf5b8f9107e61f809aca3cf1
Amit Yadav <amit931552@gmail.com>
2025-06-29 16:39:59 +0530

    sfnkbd

---
+++ src/KunalDsa/BinarySearch/FindTheMountainArray.java
@@ -0,0 +1,26 @@
+package KunalDsa.BinarySearch;
+
+import java.util.Scanner;
+
+public class FindTheMountainArray {
+
+    static  void finftheMountainArray(int[]  arr,int n){
+
+        int target=3;
+        for(int i=0; i<n; i++){
+
+            if
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[]  arr=new int[n];
+        for(int i=0; i<n; i++){
+            arr[i]=sc.nextInt();
+        }
+    }
+}
