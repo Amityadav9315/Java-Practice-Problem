37f051271ae844b7be774fcccd377a2db3f88e19
Amit Yadav <amit931552@gmail.com>
2025-06-29 08:32:25 +0530

    vfssnk

---
+++ src/KunalDsa/BinarySearch/SmallestLetterGreaterthanTarget.java
@@ -0,0 +1,31 @@
+package KunalDsa.BinarySearch;
+
+import java.util.Scanner;
+
+public class SmallestLetterGreaterthanTarget {
+
+
+
+    static  void samallestLetter(int arr,int n){
+
+
+        int start=0;
+        int
+        while(start<=end){
+
+
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        char[]  arr=new char[n];
+        for(int i=0; i<n; i++){
+
+            arr[i]=sc.next().charAt(i);
+        }
+    }
+}
