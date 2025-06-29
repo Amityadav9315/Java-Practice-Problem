c5fc20d264ac84eea8531294608842042ca74d24
Amit Yadav <amit931552@gmail.com>
2025-06-29 12:23:43 +0530

    gsfn.ksf

--- src/KunalDsa/BinarySearch/InfiniteArray.java
+++ src/KunalDsa/BinarySearch/InfiniteArray.java
@@ -4,44 +4,49 @@ import java.util.Scanner;
 
 public class InfiniteArray {
 
-    static  void binarySearch(int[] arr,int target,int start,int end){
-
-        while(start<=end){
-
-            int mid=start+(end-start)/2;
-
-            if(target==arr[mid]){
-                System.out.println();
-
-
-            }
-            else if(target>arr[mid]){
-                start=mid+1;
-            }
-            else if(target<arr[mid]){
-                end=mid-1;
-
-            }
-        }
-    }
-
-    static  void ans(int[] arr,int target){
-        int start=0;
-        int end=1;
-        while(target>arr[end]){
-            int temp=end+1;
-            end=end+(end-start+1)*2;
-            start=temp;
-        }
-
-
-
-
-    }
 
     public static void main(String[] args) {
 
+    }
+
+    static void ans(int[] arr, int target) {
+        int start = 0;
+        int end = 1;
+        while (target > arr[end]) {
+            int temp = end + 1;
+            end = end + (end - start + 1) * 2;
+            start = temp;
+        }
+        return binarySearch(arr, target, start, end);
+    }
 
 
+    static int binarySearch(int[] arr, int target, int start, int end) {
+
+        while (start <= end) {
+
+            int mid = start + (end - start) / 2;
+
+            if (target == arr[mid]) {
+                System.out.println();
+
+
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else if (target < arr[mid]) {
+                end = mid - 1;
+
+            }
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
