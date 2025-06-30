a442c95c0ca34a0a889568b3403a31197c43b7ad
Amit Yadav <amit931552@gmail.com>
2025-06-30 10:43:23 +0530

    bgdknbg

--- src/KunalDsa/RotatedSortedArray.java
+++ src/KunalDsa/RotatedSortedArray.java
@@ -8,7 +8,21 @@ public class RotatedSortedArray {
 
     }
 
-    static  void binarySearch(){
+    static  void binarySearch(int[] arr,int n,int target){
+
+        int start=0;
+        int end=arr.length;
+        while(start<=end){
+
+            int mid=start+(end-start)/2;
+            if(arr[mid]>target){
+
+            }
+            else if(arr[mid]<target){
+
+
+            }
+        }
 
     }
 
