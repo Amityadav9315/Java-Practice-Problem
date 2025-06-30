8d51c03c00633aebac875da611ae81a02f47ae36
Amit Yadav <amit931552@gmail.com>
2025-06-30 13:59:32 +0530

    sbfnksf

--- src/KunalDsa/RotatedSortedArray.java
+++ src/KunalDsa/RotatedSortedArray.java
@@ -6,9 +6,40 @@ public class RotatedSortedArray {
 
 
     public static void main(String[] args) {
-        int[] arr={4,5,6,7,0,1,2};
+        int[] arr = {4, 5, 6, 7, 0, 1, 2};
         System.out.println(findPivote(arr));
 
+
+    }
+
+
+    static int search(int[] nums, int target) {
+
+        int pivote=findPivote(nums);
+        if(pivote==-1){
+            return  binarySearch(nums,target,0, nums.length-1);
+        }
+
+
+    }
+
+
+    static  int  binarySearch(int[] arr,int target,int start,int end){
+
+        while(start<=end){
+
+            int mid=start+(end-start)/2;
+            if(target<arr[mid]){
+                end=mid-1;
+            }
+            else if(target>arr[mid]){
+                start=mid+1;
+            }
+            else{
+                return mid;
+            }
+        }
+        return -1;
 
     }
     static  int findPivote(int[] arr){
