6c87669007ff8bf9b341d16fe41f300d87ad7261
Amit Yadav <amit931552@gmail.com>
2025-06-30 10:39:08 +0530

    efvnklet

--- src/KunalDsa/BinarySearch/FindTheMountainArray.java
+++ src/KunalDsa/BinarySearch/FindTheMountainArray.java
@@ -13,7 +13,6 @@ public class FindTheMountainArray {
             if(arr[i]==target){
                 count=i;
                 break;
-                svfksf
 
 
             }
---
+++ src/KunalDsa/RotatedSortedArray.java
@@ -0,0 +1,22 @@
+package KunalDsa;
+
+import java.util.Scanner;
+
+public class RotatedSortedArray {
+
+    static  void rotatedsortedArray(){
+
+
+
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0; i<n; i++){
+            arr[i]=sc.nextInt();
+        }
+    }
+}
