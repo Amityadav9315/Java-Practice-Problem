6baa9b5425fd335b35716621ee473a23b40b7779
Amit Yadav <amit931552@gmail.com>
2025-06-30 10:40:06 +0530

    bdg

--- src/KunalDsa/RotatedSortedArray.java
+++ src/KunalDsa/RotatedSortedArray.java
@@ -6,14 +6,16 @@ public class RotatedSortedArray {
 
     static  void rotatedsortedArray(){
 
+    }
 
-
+    static  void binarySearch(){
 
     }
 
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
+        int  target=50;
         int[] arr=new int[n];
         for(int i=0; i<n; i++){
             arr[i]=sc.nextInt();
