f8ac560b20e728de062d48f99577c0a599986531
Amit Yadav <amit931552@gmail.com>
2025-07-01 15:07:38 +0530

    rgekl;er

---
+++ src/KunalDsa/BinarySearch/rowColMatrix.java
@@ -0,0 +1,31 @@
+package KunalDsa.BinarySearch;
+
+import java.util.Scanner;
+
+public class rowColMatrix {
+    static  void  rowColumnMatrix(int[][] arr,int target){
+
+        int r=0;
+        int c=arr.length-1;
+        while(r< arr.length&&c>=0){
+            if(arr[r][c]==target){
+                System.out.println(r+","+c);
+            }
+            else if(arr[r][c]<target){
+                r++;
+            }
+            else {
+                c--;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int[][] arr={{10,20,30},
+                     { 25,27,29},
+                      {28,30,35}};
+        rowColumnMatrix(arr,27);
+
+    }
+
+}
