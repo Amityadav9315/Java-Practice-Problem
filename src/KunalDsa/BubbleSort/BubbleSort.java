0b329aa5cd268f059d5d273b5b157f633e7c4df3
Amit Yadav <amit931552@gmail.com>
2025-07-02 18:49:11 +0530

    vgsblksg

---
+++ src/KunalDsa/BubbleSort/BubbleSort.java
@@ -0,0 +1,26 @@
+package KunalDsa.BubbleSort;
+
+import java.util.Scanner;
+
+public class BubbleSort {
+
+    static  void bubblesort(int[] arr,int n){{
+        int target=50;
+
+        for(int i=0; i<n; i++){
+            for(int j=)
+        }
+    }
+
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0; i<n; i++){
+            arr[i]=sc.nextInt();
+        }
+    }
+}
