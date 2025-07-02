b0f7280f32791a37774da14d4ec76dd2cb43f630
Amit Yadav <amit931552@gmail.com>
2025-07-02 22:05:46 +0530

    fnklrfnf

--- src/KunalDsa/FlippingAnImage.java
+++ src/KunalDsa/FlippingAnImage.java
@@ -1,4 +1,35 @@
 package KunalDsa;
 
+import java.util.Scanner;
+
 public class FlippingAnImage {
+
+    static  void flippingImage(int [][] arr,int n,int m){
+
+        int [][] arr1=new int[n][m];
+
+        for(int i=0; i<m; i++){
+            for(int j=0; j<m; j++){
+            }
+
+        }
+
+
+
+
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int m=sc.nextInt();
+        int[][] arr=new int[n][m];
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                arr[i][j]=sc.nextInt();
+            }
+
+        }
+    }
 }
