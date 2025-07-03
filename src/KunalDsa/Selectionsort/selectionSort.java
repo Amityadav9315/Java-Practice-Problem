206e5cb5e663a44fdaa6d5c3fb9e1e81213b74a0
Amit Yadav <amit931552@gmail.com>
2025-07-03 14:17:02 +0530

    vgh;gr

--- src/KunalDsa/Selectionsort/selectionSort.java
+++ src/KunalDsa/Selectionsort/selectionSort.java
@@ -1,14 +1,46 @@
 package KunalDsa.Selectionsort;
 
+import java.util.Scanner;
+
 public class selectionSort {
 
 
-    static  void selectionSorts(){
+    static  void selectionSorts(int[] arr,int n){
+
+
 
 
     }
+    static  void max(int[] arr,int n){
+        int max=0;
+        for(int i=0; i<n; i++){
+            if(max<arr[i]){
+                max=arr[i];
+            }
+        }
+
+    }
+
+    static  void swap(int[] arr,int n){
+
+        int temp=0;
+        int first=0;
+        int last=arr.length-1;
+        temp=arr[first];
+        arr[first]=arr[last];
+        arr[last]=temp;
+
+    }
+
 
     public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0; i<n; i++){
+            arr[i]=sc.nextInt();
+        }
 
     }
 }
