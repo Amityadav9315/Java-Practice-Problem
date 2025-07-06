ecf134e13478706229b5ec185848481dbcf13cbd
Amit Yadav <amit931552@gmail.com>
2025-07-07 00:15:41 +0530

    cfeo;

--- src/KunalDsa/Arrays/FindDuplicate.java
+++ src/KunalDsa/Arrays/FindDuplicate.java
@@ -4,7 +4,14 @@ import java.util.Scanner;
 
 public class FindDuplicate {
 
-    static  void findDuplicate(int arr){
+    static  void findDuplicate(int arr,int n){
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+
+
+            }
+        }
 
 
     }
