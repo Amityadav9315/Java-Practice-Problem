649fe43e470f6cb0f12df3354a80853d66602da6
Amit Yadav <amit931552@gmail.com>
2025-07-15 13:10:54 +0530

    fvevgrnklnklgr

---
+++ src/KunalDsa/Arrays/findpower.java
@@ -0,0 +1,27 @@
+package KunalDsa.Arrays;
+
+import java.util.Scanner;
+
+public class findpower {
+
+    static int  findPower(int x,int m){
+
+        int mult=1;
+        for(int i=0; i<m; i++){
+            mult=mult*x;
+        }
+        return mult;
+
+
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        int x=sc.nextInt();
+        int m=sc.nextInt();
+
+        System.out.println(findPower(x,m));
+    }
+}
--- src/KunalDsa/CyclicSort/FirstMissingPositive.java
+++ src/KunalDsa/CyclicSort/FirstMissingPositive.java
@@ -4,9 +4,31 @@ import java.util.Scanner;
 
 public class FirstMissingPositive {
 
-    static  void firstMissing(int arr,int n){
+    static  void firstMissing(int[] arr,int n){
 
-        int correct
+        int i=0;
+        int correct=arr[i]-1;
+        while(i<n){
+            if(arr[i]>0&&arr[i]<=n&&arr[i]!=arr[correct]){
+                int temp=arr[i];
+                arr[i]=arr[correct-1];
+                arr[correct-1]=temp;
+            }
+            else{
+                i++;
+            }
+        }
+        int ans=0;
+        for(i=0; i<n; i++){
+
+            if(arr[i]!=i+1){
+                ans=i+1;
+
+
+
+            }
+        }
+        System.out.println(ans);
 
 
     }
@@ -18,5 +40,6 @@ public class FirstMissingPositive {
         for(int i=0; i<n; i++){
             arr[i]=sc.nextInt();
         }
+        firstMissing(arr,n);
     }
 }
