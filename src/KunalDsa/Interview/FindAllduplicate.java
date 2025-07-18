111079ef84c2c3008eed1eb9f3cf77163a40e775
Amit Yadav <amit931552@gmail.com>
2025-07-18 19:36:47 +0530

    CGRN;OGR

---
+++ src/KunalDsa/Interview/FindAllduplicate.java
@@ -0,0 +1,32 @@
+package KunalDsa.Interview;
+
+import java.util.Scanner;
+
+public class FindAllduplicate {
+
+    static  void  findDuplicate(int[] arr,int n){
+
+        int count=0;
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<n; i++){
+                if(arr[i]==arr[j]){
+
+
+                }
+            }
+        }
+
+
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0; i<n;i++){
+            arr[i]=sc.nextInt();
+
+        }
+    }
+}
