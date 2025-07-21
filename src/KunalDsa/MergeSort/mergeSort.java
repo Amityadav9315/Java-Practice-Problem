1c736f109df7c2142be503eb31113df568bdeab4
Amit Yadav <amit931552@gmail.com>
2025-07-21 14:28:43 +0530

    wrongr

--- src/KunalDsa/MergeSort/mergeSort.java
+++ src/KunalDsa/MergeSort/mergeSort.java
@@ -4,14 +4,45 @@ import java.util.Scanner;
 
 public class mergeSort {
 
+    static void Conquer(int[] arr,int start,int mid,int end){
+        int n=arr.length-1;
+      int[] merged=new int[end-start+1];
+      int idx1=start;
+      int idx2=mid+1;
+      int index=0;
+      while(idx1<=mid&&idx2<=end){
+          if(arr[idx1]>=arr[idx2]){
+            merged[index++]=arr[idx2++];
+          }
+          else {
+              merged[index++]=arr[idx1++];
+          }
+      }
+
+      while(idx1<=mid){
+          merged[index++]=arr[idx1++];
+      }
+      while (idx2<=end){
+          merged[index++]=arr[idx2++];
+      }
+        for(int i=0,j=start; i<merged.length; i++,j++){
+            arr[j]=merged[i];
+        }
+
+
+
+    }
+
 
     static  void Divide(int[] arr,int start,int end){
      if(start>=end){
          return;
      }
      int mid=start+(end-start)/2;
-     Divide(arr,start,mid-1);
-     Divide(arr,);
+     Divide(arr,start,mid);
+     Divide(arr,mid+1,end);
+     Conquer(arr,start,mid,end);
+
 
     }
 
@@ -22,6 +53,10 @@ public class mergeSort {
         int[] arr=new int[n];
         for(int i=0; i<n; i++){
             arr[i]= sc.nextInt();
+        }
+        Divide(arr,0,n-1);
+        for(int i=0; i<n; i++){
+            System.out.print(arr[i]+" ");
         }
 
     }
