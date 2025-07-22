74bf419a31a6fb1d6e4c6af0c7eb934d6f551541
Amit Yadav <amit931552@gmail.com>
2025-07-22 10:23:11 +0530

    ebn

---
+++ src/KunalDsa/BasicMath/findSingleno.java
@@ -0,0 +1,30 @@
+package KunalDsa.BasicMath;
+
+import java.util.Scanner;
+
+public class findSingleno {
+
+    static  void findNumber(int [] arr,int n){
+        int unique=0;
+
+        for(int x: arr){
+            unique=unique^x;
+        }
+        System.out.println(unique);
+
+    }
+
+    public static void main(String[] args) {
+
+
+
+        Scanner sc=new Scanner(System.in);
+        int n= sc.nextInt();
+        int [] arr=new int[n];
+        for(int i=0; i<n; i++){
+            arr[i]=sc.nextInt();
+
+        }
+        findNumber(arr,n);
+    }
+}
