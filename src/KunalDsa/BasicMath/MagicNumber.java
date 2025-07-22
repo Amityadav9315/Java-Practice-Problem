ba59b7a13a22ff4148b9961d80df43e8f7d0ec29
Amit Yadav <amit931552@gmail.com>
2025-07-22 18:43:40 +0530

    ebn

---
+++ src/KunalDsa/BasicMath/MagicNumber.java
@@ -0,0 +1,31 @@
+package KunalDsa.BasicMath;
+
+import java.util.Scanner;
+
+public class MagicNumber {
+
+
+    static  void magicNumber(int n){
+        int m=0;
+        int ans=0;
+        int base=5;
+        while(n>0){
+            int last=n&1;
+            n= n>>1;
+            ans+=ans+last*base;
+            base=base*5;
+
+
+        }
+        System.out.println(ans);
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+
+        magicNumber(n);
+
+    }
+}
