6a923b815fe94f59429de337517c3197eab8014e
Amit Yadav <amit931552@gmail.com>
2025-07-24 13:34:12 +0530

    et vhlger

--- src/KunalDsa/Recursion/No1to5.java
+++ src/KunalDsa/Recursion/No1to5.java
@@ -4,16 +4,24 @@ import java.util.Scanner;
 
 public class No1to5 {
 
-    static  void number(int n){
+//    static  void number(int n){
+//        if(n==0){
+//            return;
+//        }
+//        System.out.println(n);
+//        number(n-1);
+//    }
+    static  void numberRev(int n){
         if(n==0){
             return;
         }
+
+        numberRev(n-1);
         System.out.println(n);
-        number(n-1);
     }
 
     public static void main(String[] args) {
-        number(9);
+        numberRev(9);
 
     }
 }
