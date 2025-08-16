1559ca26b1101bd16954932e5d4cf6c1f27edeff
Amit Yadav <amit931552@gmail.com>
2025-08-16 20:14:06 +0530

    grnkjl;tb

---
+++ src/Array/countFrequency.java
@@ -0,0 +1,29 @@
+package Array;
+
+import java.util.Scanner;
+
+public class countFrequency {
+    static  void frquency(int[] arr,int n){
+
+        int[] result=new int[n];
+        boolean visited;
+        for(int i=0; i<n-1; i++){
+            int count=1;
+            for(int j=i+1; j<n; j++ ){
+
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0; i<n; i++){
+            arr[i]=sc.nextInt();
+
+
+            }
+    }
+}
