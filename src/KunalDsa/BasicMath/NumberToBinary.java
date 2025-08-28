7e57bb6bdcce67718d05300e23206c10ee87ee6a
Amit Yadav <amit931552@gmail.com>
2025-08-28 22:41:48 +0530

    ces gl

--- src/KunalDsa/BasicMath/NumberToBinary.java
+++ src/KunalDsa/BasicMath/NumberToBinary.java
@@ -31,7 +31,6 @@ public class NumberToBinary {
                 System.out.println("Programme stopped");
                 break;
             }
-            vhhvbgkbjk
 
             numberTobInary(n);
 
